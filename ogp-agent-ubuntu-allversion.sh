83b130ebd4ca8c8985f26ccacb0ecc5e53726e56
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2019-07-19 10:12:16 +0530

    upload

---
+++ ogp-agent-ubuntu-allversion.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo apt-get update && apt-get upgrade -y
+sudo apt-get install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl -y
+sudo apt-get install libc6-i386
+sudo apt-get install libgcc1:i386
+sudo apt-get install lib32gcc1
+sudo apt-get install libhttp-daemon-perl
+wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
+sudo dpkg -i "ogp-agent-latest.deb"
+sudo cat /root/ogp_user_password
+
+echo "OGP Agent Installed Successfully"
---
+++ ogp-panel-ubuntu18.04.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+apt-get update && apt-get upgrade -y &&
+sudo apt-get install apache2 curl subversion php7.2 php7.2-gd php7.2-zip libapache2-mod-php7.2 php7.2-curl php7.2-mysql php7.2-xmlrpc php-pear phpmyadmin mysql-server php7.2-mbstring php-gettext git php-bcmath -y
+sudo mysql_secure_installation
+wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-panel-latest.deb" -O "ogp-panel-latest.deb"
+sudo dpkg -i "ogp-panel-latest.deb"
+sudo cat /root/ogp_panel_mysql_info 
+sudo cat /root/ogp_user_password 
