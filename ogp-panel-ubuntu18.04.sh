6d50baf7cd06d95fde01b96d6e36f75fc1fdf9aa
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2019-12-23 16:29:48 +0530

    update

--- ogp-panel-ubuntu18.04.sh
+++ ogp-panel-ubuntu18.04.sh
@@ -1,40 +1,10 @@
-#!/usr/bin/env bash
+sudo apt-get install apache2 curl subversion php7.2 php7.2-gd php7.2-zip libapache2-mod-php7.2 php7.2-curl php7.2-mysql php7.2-xmlrpc php-pear phpmyadmin mysql-server php7.2-mbstring php-gettext git php-bcmath -y
 
-greenMessage() {
-	echo -e "\\033[32;1m${@}\033[0m"
-}
+sudo mysql_secure_installation
 
-update(){
-	greenMessage "** Updating.."
-	apt update && apt upgrade -y
-}
+wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-panel-latest.deb" -O "ogp-panel-latest.deb"
+sudo dpkg -i "ogp-panel-latest.deb"
 
-install_des(){
-	greenMessage "** Installing Required Dependencies.."
-	sudo apt-get install apache2 curl subversion php7.2 php7.2-gd php7.2-zip libapache2-mod-php7.2 php7.2-curl php7.2-mysql php7.2-xmlrpc php-pear phpmyadmin mysql-server php7.2-mbstring php-gettext git php-bcmath -y
-}
+sudo cat /root/ogp_panel_mysql_info 
 
-mysql_setup(){
-	greenMessage "** MySQL Setup.."
-	sudo mysql_secure_installation
-}
-
-get_files(){
-	greenMessage "Downloading Files & Extracting.."
-	wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-panel-latest.deb" -O "ogp-panel-latest.deb"
-	sudo dpkg -i "ogp-panel-latest.deb"
-}
-
-get_info(){
-	greenMessage "** Open Game Panel Info.."
-	sudo cat /root/ogp_panel_mysql_info 
-	sudo cat /root/ogp_user_password 
-}
-
-update
-install_des
-mysql_setup
-get_files
-get_info
-
-greenMessage "** OGP Panel Installed!"
+sudo cat /root/ogp_user_password
