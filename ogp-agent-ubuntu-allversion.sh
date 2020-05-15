26e854cac8e67de89f96764cda4de86c31786242
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2020-05-15 23:31:14 +0530

    update

--- ogp-agent-ubuntu-allversion.sh
+++ ogp-agent-ubuntu-allversion.sh
@@ -1,10 +1,13 @@
-sudo apt-get update;
-sudo apt-get upgrade;
-sudo apt-get install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl -y;
-sudo apt-get install libc6-i386;
-sudo apt-get install libgcc1:i386;
-sudo apt-get install lib32gcc1;
-sudo apt-get install libhttp-daemon-perl;
-wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb";
-sudo dpkg -i "ogp-agent-latest.deb";
-sudo cat /root/ogp_user_password;
+#!/bin/bash
+# Installing OGP Agent on all ubuntu versions server
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl -y
+sudo apt install libc6-i386 -y
+sudo apt install libgcc1:i386 -y
+sudo apt install lib32gcc1 -y
+sudo apt install libhttp-daemon-perl -y
+wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
+sudo dpkg -i "ogp-agent-latest.deb"
+sudo cat /root/ogp_user_password
