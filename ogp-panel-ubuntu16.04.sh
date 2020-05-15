89107da13d9568cf9af630531ec60e2a6f554c2a
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2020-05-15 23:38:26 +0530

    update

--- ogp-panel-ubuntu16.04.sh
+++ ogp-panel-ubuntu16.04.sh
@@ -1,40 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Installing OGP Panel on ubuntu 16.04 server
 
-greenMessage() {
-	echo -e "\\033[32;1m${@}\033[0m"
-}
-
-update(){
-	greenMessage "** Updating.."
-	apt update && apt upgrade -y
-}
-
-install_des(){
-	greenMessage "** Installing Required Dependencies.."
-	sudo apt-get install apache2 curl subversion php7.0 php7.0-gd php7.0-zip libapache2-mod-php7.0 php7.0-curl php7.0-mysql php7.0-xmlrpc php-pear phpmyadmin mysql-server php7.0-mbstring php-gettext git php-bcmath -y
-}
-
-mysql_setup(){
-	greenMessage "** MySQL Setup.."
-	sudo mysql_secure_installation
-}
-
-get_files(){
-	greenMessage "Downloading Files & Extracting.."
-	wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-panel-latest.deb" -O "ogp-panel-latest.deb"
-	sudo dpkg -i "ogp-panel-latest.deb"
-}
-
-get_info(){
-	greenMessage "** Open Game Panel Info.."
-	sudo cat /root/ogp_panel_mysql_info 
-	sudo cat /root/ogp_user_password 
-}
-
-update
-install_des
-mysql_setup
-get_files
-get_info
-
-greenMessage "** OGP Panel Installed!"
+apt update
+apt upgrade -y
+sudo apt-get install apache2 curl subversion php7.0 php7.0-gd php7.0-zip libapache2-mod-php7.0 php7.0-curl php7.0-mysql php7.0-xmlrpc php-pear phpmyadmin mysql-server php7.0-mbstring php-gettext git php-bcmath -y
+sed -i "s/^bind-address.*/bind-address=0.0.0.0/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
+sudo mysql_secure_installation
+wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-panel-latest.deb" -O "ogp-panel-latest.deb"
+sudo dpkg -i "ogp-panel-latest.deb"
+sudo cat /root/ogp_panel_mysql_info
+sudo cat /root/ogp_user_password
