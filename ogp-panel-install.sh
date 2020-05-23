1e1e20d2467bf6fedd432527e4402ec9d1d3ce05
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2020-05-23 17:37:43 +0530

    Update ogp-panel-install.sh

--- ogp-panel-install.sh
+++ ogp-panel-install.sh
@@ -3,35 +3,6 @@
 # github.com/sanjaysrocks
 # https://github.com/SanjaySRocks/OGP-Installer/blob/master/ogp-panel-install.sh
 
-echo "* Select Your OS"
-echo "1) Ubuntu 20.04"
-echo "2) Ubuntu 18.04"
-echo "3) Ubuntu 16.04"
-echo "4) Debian 10"
-echo "5) Debian 9"
-echo "6) Debian 8"
-echo "7) Quit"
-
-read -p "Where you want to install? (1/2/3/4/5/6) or 7 to Quit : " n
-
-case $n in
-  1) ubuntu_2004
-	;;
-  2) ubuntu_1804
-	;;
-  3) ubuntu_1604
-	;;
-  4) debian_10
-	;;
-  5) debian_9
-	;;
-  6) debian_8
-	;;
-  7) exit ;;
-  *) echo "invalid option please enter 1/2/3/4 value only"; exit;
-	;;
-esac
-
 ubuntu_2004()
 {
 	apt update
@@ -74,6 +45,29 @@ debian_8()
 	apt upgrade -y
 	sudo apt install -y apache2 curl subversion php5 php5-gd php5-xmlrpc php5-curl php5-mysql php-pear phpmyadmin mysql-server libapache2-mod-php5 git
 }
+
+echo "* Select Your OS"
+echo "1) Ubuntu 20.04"
+echo "2) Ubuntu 18.04"
+echo "3) Ubuntu 16.04"
+echo "4) Debian 10"
+echo "5) Debian 9"
+echo "6) Debian 8"
+echo "7) Quit"
+
+read -p "Where you want to install? (1/2/3/4/5/6) or 7 to Quit : " n
+
+case $n in
+  1) ubuntu_2004;;
+  2) ubuntu_1804;;
+  3) ubuntu_1604;;
+  4) debian_10;;
+  5) debian_9;;
+  6) debian_8;;
+  7) exit;;
+  *) echo "invalid option please enter 1/2/3/4 value only"; exit;
+	;;
+esac
 
 sed -i "s/^bind-address.*/bind-address=0.0.0.0/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 sudo mysql_secure_installation
