125dc5e77ec310e0dcbb424e5e8835ef16084f09
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2020-05-26 18:22:47 +0530

    update

--- install-agent.sh
+++ install-agent.sh
@@ -5,11 +5,11 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl -y
-sudo apt install libc6-i386 -y
-sudo apt install libgcc1:i386 -y
-sudo apt install lib32gcc1 -y
-sudo apt install libhttp-daemon-perl -y
+sudo apt -y install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl
+sudo apt -y install libc6-i386
+sudo apt -y install libgcc1:i386
+sudo apt -y install lib32gcc1
+sudo apt -y install libhttp-daemon-perl
 wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
 sudo dpkg -i "ogp-agent-latest.deb"
 sudo cat /root/ogp_user_password
