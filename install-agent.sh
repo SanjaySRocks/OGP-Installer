492d3804b2430fff37ccac1a6878a60f9d1837ec
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2021-01-02 11:10:51 +0530

    update

--- install-agent.sh
+++ install-agent.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 # Installing OGP Agent on all ubuntu versions server
 # github.com/sanjaysrocks
-# https://github.com/SanjaySRocks/OGP-Installer/blob/master/ogp-agent-install.sh
+# https://github.com/SanjaySRocks/OGP-Installer/blob/master/install-agent.sh
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt -y install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl
-sudo apt -y install libc6-i386
-sudo apt -y install libgcc1:i386
-sudo apt -y install lib32gcc1
-sudo apt -y install libhttp-daemon-perl
-wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
-sudo dpkg -i "ogp-agent-latest.deb"
+install_packages(){
+    sudo apt -y update
+    sudo apt -y upgrade
+    sudo apt -y install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl
+    sudo apt -y install libc6-i386
+    sudo apt -y install libgcc1:i386
+    sudo apt -y install lib32gcc1
+    sudo apt -y install libhttp-daemon-perl
+} &> /dev/null
+
+install_agent(){
+    wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
+    sudo dpkg -i "ogp-agent-latest.deb"
+} &> /dev/null
+
+echo "Installing Packages..."
+install_packages
+
+echo "Downloading and Installing Agent..."
+install_agent
 sudo cat /root/ogp_user_password
+
+echo "Installation Completed !"
