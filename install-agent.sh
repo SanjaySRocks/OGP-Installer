6b4c6add720430f0b89542f7b7dfce3f50ce8137
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2021-01-02 11:12:32 +0530

    version 2

--- install-agent.sh
+++ install-agent.sh
@@ -3,9 +3,12 @@
 # github.com/sanjaysrocks
 # https://github.com/SanjaySRocks/OGP-Installer/blob/master/install-agent.sh
 
-install_packages(){
+update_system(){
     sudo apt -y update
     sudo apt -y upgrade
+} &> /dev/null
+
+install_packages(){
     sudo apt -y install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion libarchive-extract-perl pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl
     sudo apt -y install libc6-i386
     sudo apt -y install libgcc1:i386
@@ -17,6 +20,9 @@ install_agent(){
     wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
     sudo dpkg -i "ogp-agent-latest.deb"
 } &> /dev/null
+
+echo "Updating..."
+update_system
 
 echo "Installing Packages..."
 install_packages
